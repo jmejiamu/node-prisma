4063eaef15e0472b9b8bda36543670ba4601017d
Jose <jmejiamu@gmail.com>
2021-10-02 23:20:16 -0700

    configure the env used

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 
+const envconfig = require('dotenv');
+
+//Configuration to use the .env file 
+envconfig.config();
+
 const app = express();
 
 app.use(express.json());
 
+// Root End point
 app.get('/', async (req, res) => {
     res.json({ endpoint: "Hello, from the root end point" })
 })
 
-
+// port where the server is listen
 const port = process.env.PORT || "3000";
 
 app.listen(port, () => {
